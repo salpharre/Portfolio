cefa4cb347e5436857e9076298c36f0951458d79
Sandra <sandra.alpha2010@gmail.com>
2020-08-25 14:27:53 -0700

    home page renders correctly

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,7 +1,48 @@
 import React from "react";
+import img1 from "../../images/compressed/beyondvacation.jpg";
+import img2 from "../../images/compressed/weather.jpg";
+import img3 from "../../images/compressed/directory.jpg";
+
 
 function Carousel() {
-
+    return (
+        <div className="jumbotron welcome">
+            <div id="carouselNext" className="carousel slide carousel-fade" data-ride="carousel">
+                <ol className="carousel-indicators dark">
+                    <li data-target="#carouselNext" data-slide-to="0" className="active"></li>
+                    <li data-target="#carouselNext" data-slide-to="1"></li>
+                    <li data-target="#carouselNext" data-slide-to="2"></li>
+                </ol>
+                <div className="carousel-inner">
+                    <div className="carousel-item active" data-interval="3000">
+                        <a id="linkToPosition" href="./html/portfolio.html#linkToProject2">
+                            <img src={img1} className="d-block w-100 img-fluid"
+                            alt="Vacation Outdoor Rental App Main Page"></img>
+                        </a>
+                    </div>
+                    <div class="carousel-item" data-interval="3000">
+                        <a class="linkToPosition" href="./html/portfolio.html#linkToEmployee">
+                            <img src={img3} class="d-block w-100 img-fluid"
+                            alt="Employee Directory"></img>
+                        </a>
+                    </div>
+                    <div class="carousel-item" data-interval="3000">
+                        <a class="linkToPosition" href="./html/portfolio.html#linkToWeather">
+                            <img src={img2} class="d-block w-100 img-fluid" alt="Weather Dashboard"></img>
+                        </a>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselNext" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselNext" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default Carousel;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,14 @@
 import React from "react";
-// import Jumbotron from "../components/Jumbotron";
 import Header from "../components/Header";
 import Welcome from "../components/Welcome";
-// import Carousel from "../components/Carousel";
+import Carousel from "../components/Carousel";
 
 function Home() {
     return (
         <div>
             <Header />
             <Welcome />
-            {/*<Carousel />*/}
+            <Carousel />
         </div>
     )
 };
