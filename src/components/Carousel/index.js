ac21c5f20a149e6d6995d95e07ec08448877d74a
Sandra <sandra.alpha2010@gmail.com>
2020-08-29 09:49:29 -0700

    added z-index to indicators and changed other z-index values to corrolate with the new z-index, indicators now don't scroll over footer

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -7,18 +7,23 @@ import "./style.css";
 function Carousel() {
     return (
         <div className="welcome caro">
-            <div id="carouselNext" className="carousel slide carousel-fade" data-ride="carousel">
+            <div id="carouselNext indicatorParent" className="carousel slide carousel-fade" data-ride="carousel">
+                <ol className="carousel-indicators dark">
+                    <li data-target="#carouselNext" data-slide-to="0" className="active"></li>
+                    <li data-target="#carouselNext" data-slide-to="1"></li>
+                    <li data-target="#carouselNext" data-slide-to="2"></li>
+                </ol>
                 <div className="carousel-inner">
                     <div className="carousel-item active" data-interval="3000">
-                            <img src={img1} className="d-block w-100 img-fluid"
+                        <img src={img1} className="d-block w-100 img-fluid"
                             alt="Vacation Outdoor Rental App Main Page"></img>
                     </div>
                     <div className="carousel-item" data-interval="3000">
-                            <img src={img3} className="d-block w-100 img-fluid"
+                        <img src={img3} className="d-block w-100 img-fluid"
                             alt="Employee Directory"></img>
                     </div>
                     <div className="carousel-item" data-interval="3000">
-                            <img src={img2} className="d-block w-100 img-fluid" alt="Weather Dashboard"></img>
+                        <img src={img2} className="d-block w-100 img-fluid" alt="Weather Dashboard"></img>
                     </div>
                 </div>
                 <a className="carousel-control-prev" href="#carouselNext" role="button" data-slide="prev">
@@ -36,3 +41,4 @@ function Carousel() {
 
 export default Carousel;
 
+/*removed indicators as they continued to scroll over footer,*/
